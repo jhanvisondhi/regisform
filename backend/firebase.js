9e485e96542fe209460b0c09f6c350e40ac39c15
B GOURAV <njrgourav@gmail.com>
2024-12-06 15:12:23 +0530

    initial commit

--- backend/firebase.js
+++ backend/firebase.js
@@ -3,15 +3,15 @@ import firebase from "firebase/app";
 import "firebase/auth"; // Import Firebase Authentication if you need it
 import "firebase/firestore"; // Import Firebase Firestore if you need it
 
-// Your Firebase configuration
+// Use environment variables for Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBbdPQclhUYNtMK4qJh0nJ-5QhXcHtlh1w",
-  authDomain: "nist-a4a42.firebaseapp.com",
-  projectId: "nist-a4a42",
-  storageBucket: "nist-a4a42.appspot.com",
-  messagingSenderId: "659627474379",
-  appId: "1:659627474379:web:0a5a7c15754762a5680a4e",
-  measurementId: "G-4NW41XYGKW"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
